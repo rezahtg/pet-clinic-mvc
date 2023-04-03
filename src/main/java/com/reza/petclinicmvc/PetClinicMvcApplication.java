b6d62dc98d9fc4c8004e57e60a024130633cfedd
reza hutagaol <silentsound05@gmai.com>
2023-04-03 15:05:44 +0700

    initial-commit

---
+++ src/main/java/com/reza/petclinicmvc/PetClinicMvcApplication.java
@@ -0,0 +1,13 @@
+package com.reza.petclinicmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reza/petclinicmvc/PetClinicMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reza.petclinicmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
